193560fef2f3e4755617d11ddf32908b3453e1ad
elaydg <elayd.gg@gmail.com>
2024-05-20 16:27:12 +0300

    health bar
    can barı scene olarak kaydedildi

--- Assets/Scripts/healthBar.cs
+++ Assets/Scripts/healthBar.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class healthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Slider healthSlider;
+    public float maxHealth = 100f;
+    public float health;
     void Start()
     {
-        
+        health = maxHealth; 
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (healthSlider.value != health) 
+        {
+            healthSlider.value = health;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            takeDamage(10);
+        }
+    }
+
+    void takeDamage(float damage) 
+    {
+        health -= damage;
     }
 }
